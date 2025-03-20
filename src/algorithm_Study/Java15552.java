9d51d60bc43d9b7941f085ace6952402630367dd
Jeewon Koo <jeewon514@gmail.com>
2025-03-20 10:52:28 +0900

    반복문Level6

--- src/algorithm_Study/Java15552.java
+++ src/algorithm_Study/Java15552.java
@@ -4,8 +4,8 @@ package algorithm_Study;
 
 import java.io.*;
 
-public class Java152 {
-	public static voi55d main(String[] args) throws IOException {
+public class Java15552 {
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
