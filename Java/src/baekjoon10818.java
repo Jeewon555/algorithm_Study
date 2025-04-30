3b7848a8a57e78029fb94313de57409d4cea378c
Jeewon Koo <jeewon514@gmail.com>
2025-04-30 12:18:00 +0900

    Java

---
+++ Java/src/baekjoon10818.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class baekjoon10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        //입력값을 저장할 배열 생성
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //처음값으로 초기화
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+            }
+            if (arr[i] >= max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.print(min + " " + max);
+        sc.close();
+    }
+}
